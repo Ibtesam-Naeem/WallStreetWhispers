ba254822996fc984b69f735df5e10a707b34bc54
Ibtesam Naeem <ibtesamnaeem@gmail.com>
2025-03-13 17:30:14 -0400

    - Fixed

--- src/config/api_client.py
+++ src/config/api_client.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 def get_economic_events(limit: int = 10) -> List[Dict[str, Any]]:
     """
